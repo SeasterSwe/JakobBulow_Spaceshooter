a04b473530ca1b4a9959d575aee5d2f07c075435
Jakob Bulow <jakobbulow01@gmail.com>
2018-10-06 02:30:03 +0200

    All Enemies Done and Ready
    Yeah, ba att fixa så man ej behöver bullet

---
+++ Assets/Scripts/DodgingEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgingEnemy : shootingEnemy {
+
+    [Range(-5, 5)]
+    public float swaySpeed, swayStregth;
+
+    public override void Move()
+    {
+        SwayEffect();
+    }
+    public void SwayEffect()
+    {
+        rb2.velocity = new Vector2(-speed, Mathf.Sin(swayStregth * Time.time) * swaySpeed);
+    }
+}
---
+++ Assets/stage3Dodge_Fire.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stage3Dodge_Fire : shootingEnemy {
+
+}
